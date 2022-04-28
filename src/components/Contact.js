3e3ee611be1150f4e53c97c73732a7be47a40a95
jmusher <jeremy.musher@colorado.edu>
2022-04-28 09:05:09 -0600

    projects info added, contact links added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,22 @@ function Contact() {
     return (
     <div className="contact">
         <h1>Contact</h1>
+        <div className="contact-section">
+            <div className="contact-blurb">
+                <p>ispum lipsum etc</p>
+            </div>
+        </div>
+        <div className="contact-link-holder">
+            <div className="contact-link">
+                <a href="mailto:musher516@gmail.com">musher516@gmail.com</a>
+            </div>
+            <div className="contact-link">
+                <a href="https://www.linkedin.com/in/jmusher/" target="_blank" rel="noreferrer">LinkedIn</a>
+            </div>
+            <div className="contact-link">
+                <a href="https://github.com/jeremymusher" target="_blank" rel="noreferrer">GitHub</a>
+            </div>
+        </div>
     </div>
     )
 }
